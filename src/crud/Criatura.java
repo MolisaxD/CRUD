aabb09e2e2e7dd10d126de0cbae31c106f1ec2ca
MELISSA SOUZA BORGES <mborges@labcetec.net>
2023-10-18 11:23:34 -0300

    a

---
+++ src/crud/Criatura.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package crud;
+
+/**
+ *
+ * @author mborges
+ */
+public class Criatura {
+    public String nome, especie, classe, alimentacao;
+    public char sexo;
+    public int idade; 
+}
